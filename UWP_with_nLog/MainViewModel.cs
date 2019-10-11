4d848dcc3463c8145f9e01c89cd47c8eb21c595c
Anna Melashkina <melashkina@outlook.com>
2019-10-11 17:09:57 +0200

    first commit

---
+++ UWP_with_nLog/MainViewModel.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UWP_with_nLog
+{
+    public class MainViewModel
+    {
+        private readonly ILogger _logger;
+        public MainViewModel(ILogger<MainViewModel> logger)
+        {
+            _logger = logger;
+
+            _logger.LogDebug("I'm not bad :( I'm not gonna crash our app in InitializeComponent call");
+        }
+    }
+}
